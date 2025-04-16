53da931532cd9d566af2e663b78fa8a6fcb10049
Thomas Lunnen III <60558126+wLunnen@users.noreply.github.com>
2025-04-16 13:43:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+/*
 //card logic
 document.addEventListener("DOMContentLoaded", () => {
   // Placeholder: Interactive card logic to be implemented
 });
-
+document.getElementByClassName('card') {
+  onClick="function(){
+    ;
+    return false;
+};
+*/
